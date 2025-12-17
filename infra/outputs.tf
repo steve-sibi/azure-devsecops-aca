216074864e40b2803e1493e3b1aa01fc534d38eb
Steve Sibi <steve.sibi@gmail.com>
2025-12-17 13:02:53 -0600

    Update FastAPI output to include HTTPS URL format

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,6 +1,6 @@
 output "fastapi_url" {
-  value       = try(azurerm_container_app.api[0].ingress[0].fqdn, null)
-  description = "FastAPI public FQDN"
+  value       = try("https://${azurerm_container_app.api[0].ingress[0].fqdn}", null)
+  description = "FastAPI public URL (HTTPS)"
 }
 
 output "sb_queue_id" {
