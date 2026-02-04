67be754661c86448ad11e4c29f1314ae26eb4441
Steve Sibi <steve.sibi@gmail.com>
2026-02-04 14:11:06 -0600

    feat: add comment to clarify Web PubSub SKU usage for cost management

--- infra/webpubsub.tf
+++ infra/webpubsub.tf
@@ -1,5 +1,6 @@
 # Web PubSub for real-time scan updates
 resource "azurerm_web_pubsub" "wps" {
+  #checkov:skip=CKV_AZURE_175: Using Free_F1 to minimize cost for now (non-prod); upgrade to Standard/Premium when SLA is required.
   name                = local.webpubsub_name
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
